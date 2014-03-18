9abac0f1967105310447a421cfffeea535e69e61
Ilana Sufrin <ilana.sufrin@huffingtonpost.com>
2014-03-17 21:13:23 -0400

    initial victorian pen name commit

---
+++ victorian.js
@@ -0,0 +1,212 @@
+(function($) {
+    $(function() {
+        var prev = 0;
+        var quotes = [
+            { text: "Don't ask me about her again. Don't ask me about her again. Don't ask me...", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1414.mp3", image: "http://i.imgur.com/JTkswXT.jpg" },
+            { text: "What kind of question is that? It didn't make sense.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition33.mp3", image: "http://i.imgur.com/qQe6D8o.jpg" },
+            { text: "I don't have to listen to anything you have to say.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition11.mp3", image: "http://i.imgur.com/MJVBu4j.jpg" },
+            { text: "I don't know if I've been to Australia. Have I been to Australia?", audio: "https://bieber.s3.amazonaws.com/bieber+deposition22.mp3", image: "http://i.imgur.com/6p5ahCI.jpg" },
+            { text: "Guess what? I don't recall.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1212.mp3", image: "http://i.imgur.com/JTkswXT.jpg" },
+            { text: "What kind of question is that? Is he my son?", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1111.mp3", image: "http://i.imgur.com/qQe6D8o.jpg" },
+            { text: "I think my lawyer's asking a question.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1010.mp3", image: "http://i.imgur.com/MJVBu4j.jpg" },
+            { text: "This is a film? This is a film?", audio: "https://bieber.s3.amazonaws.com/bieber+deposition88.mp3", image: "http://i.imgur.com/6p5ahCI.jpg" },
+            { text: "I object.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition77.mp3", image: "http://i.imgur.com/JTkswXT.jpg" },
+            { text: "Journalism, this isn't journalism?", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1313.mp3", image: "http://i.imgur.com/qQe6D8o.jpg" },
+            { text: "I don't know Katie Couric, you tell me.", audio: "https://bieber.s3.amazonaws.com/bieber+deposition1515.mp3", image: "http://i.imgur.com/MJVBu4j.jpg" }
+        ];
+
+        //not sure whether I should make an image for every name choice in an object or just write the word on the page
+        var firstNameChoices = [
+           ["Acton","Arnold","Augustus","Aberforth"],
+           ["Bertram", "Benjamin", "Byron", "Burton", "Bartholomew"],
+           ["Cecil", "Charles", "Chester", "Cornelius", "Cyrus", "Clement"],
+           ["Daniel", "Denton", "Dalton", "Dawson"],
+           ["Edmund", "Egbert", "Ernest", "Ellis"],
+           ["Fitzwilliam", "Frederick", "Francis", "Frank"],
+           ["Garfield", "Gordon", "Gerard", "George", "Gilbert"],
+           ["Hugh", "Harry", "Henry", "Herbert"],
+           ["Ignatius", "Isaac", "Ingram", "Isidore"],
+           ["Jeremiah", "Jasper", "John", "Julian", "Jacob"],
+           ["Kenton", "Kingsley", "Kirk"],
+           ["Lionel", "Laurence", "Leland", "Leonard"],
+           ["Matthew", "Maximilian", "Meriwether", "Mortimer"],
+           ["Nicholas", "Norbert", "Neil"],
+           ["Oscar", "Oliver", "Owen", "Oswald"],
+           ["Paul", "Percy", "Phillip","Peter"],
+           ["Quentin", "Quincy"],
+           ["Raymond","Rodney","Roger","Ralph","Richard"],
+           ["Sacheverell","Simon","Sinclair","Stephen"],
+           ["Theodore","Thomas","Terrence"],
+           ["Upton","Ulysses"],
+           ["Victor","Vaughn","Virgil","Vincent"],
+           ["Wesley","Wallace","Wendell","Weldon","William"],
+           ["Xavier","Xander"],
+           ["Yancy","York"],
+           ["Zebediah","Zachary"]
+        ];
+
+        var lastNameChoices = [
+           ["Adams", "Ashdown", "Alcorn", "Ailey"],
+           ["Bell", "Bamford", "Berry", "Beaton", "Black", "Blewett", "Burrows", "Butler"],
+           ["Carroll", "Cockerfell","Collins","Cunningham","Copperfield"],
+           ["Dalton","Darcy","Davidson","Dickens","Dickenson","Drummond"],
+           ["English","Evans","Eliot"],
+           ["Fletcher","Fezziwig","Fred","Faggins"],
+           ["Gaskell","Gibbons","Goodwood","Gilmore","Gargery"],
+           ["Harding","Hardy","House","Holloway","Havisham"],
+           ["Ivers","Irvin","Isherwood","Infelfinger"],
+           ["James","Jefferies","Jaggers","Jarvis","Jorkins"],
+           ["Keefe","Krook","Kedgick","Kenfield"],
+           ["Lawrence","Little","Lockaby","Lansdown"],
+           ["Mitchell","Morrell","Magnus","Marlowe"],
+           ["Nickleby","Northcutt","Nightingale","Nottingham"],
+           ["Ockleberry","Orlick","Oliver"],
+           ["Pockett","Perkins","Peabody","Plympton"],
+           ["Quin","Quick","Quimby"],
+           ["Radfoot","Rathburn","Reeves","Rawlings","Rochester"],
+           ["Sandiford","Samuelson","Scoville","Sedgwick","Somerset"],
+           ["Tarkington","Tulliver","Thistlethwaite","Throgmorton"],
+           ["Upshaw", "Underhill","Umble"],
+           ["Vickers","Vosper","Venable"],
+           ["Webster","Weston","Warburton","Wickham"],
+           ["Exton","Exley"],
+           ["Yeates","Yarbrough","Yearwood"],
+           ["Shillington","Shelburn","Shreeves"]
+        ];
+        
+        var bieberPeep = function(cb) {
+            $(".bieber-peep").animate({
+                bottom: "-40"
+            }, 1000, cb);    
+        };
+        
+        var stateReset = function() {
+            $(".bieber-wait, .bieber-result").hide();
+            $(".bieber-ask").show();
+            $(".bieber-input").one('keypress', function() { bieberPeep(); }).val("");
+        };
+        
+        var stateWait = function(cb) {
+            $(".bieber-ask").fadeOut(function() {
+               $(".bieber-wait").fadeIn(cb); 
+            });
+        };
+        
+
+        var pickName = function() {
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            console.log(firstName + " " + lastName);
+            var firstLetter = (firstName.charAt(0)).toLowerCase();
+            var lastLetter = (lastName.substring(0,1)).toLowerCase();
+            
+            window.newFirstName = firstNameChoices[3][1];
+           // alert(newFirstName);
+            window.newLastName = lastNameChoices[2][1];
+            //alert(newLastName);
+
+            if (firstLetter == "a") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[0].length)));
+                newFirstName = firstNameChoices[0][test1];
+                alert(newFirstName);
+            }
+             else if (firstLetter == "b") {
+                var test1 = (Math.floor(Math.random() * (firstNameChoices[1].length)));
+                newFirstName = firstNameChoices[1][test1];
+                alert(newFirstName);
+            }
+            if (lastLetter == "a") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[0].length)));
+                newLastName = lastNameChoices[0][test2];
+                alert(newLastName);
+            }
+            else if (lastLetter == "b") {
+                var test2 = (Math.floor(Math.random() * (lastNameChoices[1].length)));
+                newLastName = lastNameChoices[1][test2];
+                alert(newLastName);
+            }
+        };
+
+
+
+        var stateAnswer = function(c) {
+            window.bieberQuote = quotes[c];
+
+            $(".bieber-wait").fadeOut(function() {
+                $(".bieber-quote").text(quotes[c].text);
+                $(".bieber-image").attr("src", quotes[c].image);
+              //  $(".bieber-audio").empty().append($("<audio />", { autoplay: true }).append($("<source />", { type: "audio/mpeg", src: (Math.random() > 0.95 ? "http://www.w3schools.com/tags/horse.mp3" : quotes[c].audio) })));
+                $(".bieber-result").fadeIn();
+            });
+        };
+        
+        stateReset();
+        
+        $(".bieber-reset").click(function() {
+            stateReset();
+            return false;
+        });
+        
+        $(".bieber-button").click(function() {
+            //pickName();
+            window.bieberQuestion = $(".bieber-input").val();
+            if (window.bieberQuestion.length < 3) {
+                alert("Your publishers need inspiration from a real name!");
+                return false;
+            }
+            
+            stateWait(function() { 
+                setTimeout(function() {
+                    stateAnswer(Math.floor(Math.random() * quotes.length));
+                    pickName();
+                }, Math.round(Math.random() * 2000 + 1000));
+            });
+                       
+            return false; 
+        });
+    });
+})(jQuery);
+
+
+
+window.fbShareBieber = function() {
+            var choice = window.bieberQuote;
+            FB.ui({
+                method: 'feed',
+                name: "Victorian Pen Name Generator",
+                picture: choice.image,
+                link: window.location.href,
+                caption: choice.text,
+                description: 'Get a pseudonym from the publishers!',
+               // description: 'I asked "' + window.bieberQuestion + '" Type any question in to our HuffPost Celebrity original interactive below to see how the Biebs himself would respond.',
+            }, 
+            function(response) {
+                if (response && response.post_id) {
+                    console.log('FB quiz post was published.');
+                }
+            });
+        };
+
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : "1427100424195799",
+        status     : true,
+        xfbml      : true
+    });
+};
+
+(function(d, s, id){var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) {return;}js = d.createElement(s); js.id = id;js.src = "http://connect.facebook.net/en_US/all.js";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+
+    function getText(text) {
+        if (text.length > 49) {
+            return (text.substring(0,47) + '...');
+        } else {
+            return text;
+        }
+    }
+
+    window.twitterShareBieber = function(quiz, text) {
+        var twitterURL = 'https://twitter.com/share?url=http://huff.to/1fprAj6&text=I got: "' + getText(window.bieberQuote.text) + '" Get your own &hashtags=VictorianPenName';
+        window.open(twitterURL, 'targetWindow','toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=300,height=300');
+    };
